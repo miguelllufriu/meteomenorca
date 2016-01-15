89647dec781066df911e0f361a73bf05cb6414b2
miguel llufriu <miguel llufriu>
2016-01-15 12:40:00 +0100

    Millora d'estructura, s'afegeix 'ICONO' i es prepara footer

--- application/controllers/home_controller.php
+++ application/controllers/home_controller.php
@@ -1,18 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home_controller extends CI_Controller {
 
 	function __construct()
 	{
 		parent::__construct();
 		$this->load->helper('url');
-    $this->load->model('homeData_model');
+		$this->load->library('user_agent');
+    $this->load->model('home_model');
 	}
 
 	public function index()
 	{
-		$datos['datosPrincipales'] = $this->homeData_model->getUltimoRegistro();
+		$datos['datosPrincipales'] = $this->home_model->getUltimoRegistro();
 		$this->load->view('templates/header.html');
 		$this->load->view('home.html',$datos);
 		$this->load->view('templates/footer.html');
--- application/models/home_model.php
+++ application/models/home_model.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class HomeData_model extends CI_Model {
+class Home_model extends CI_Model {
 
   function __construct()
   {
