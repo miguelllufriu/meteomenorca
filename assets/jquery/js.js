395f3c43786dd4481f5afb79a526ac3df59b73f6
Miguel Llufriu <miguelllufriu@gmail.com>
2016-01-26 01:16:22 +0100

    Afegit background

--- assets/jquery/js.js
+++ assets/jquery/js.js
@@ -2,6 +2,26 @@ $( document ).ready(function(){
 
     /*Predefinido por materialize para inicializar menu en disp. móviles*/
    $(".button-collapse").sideNav();
-
+   /*Imagen de fondo según hora del día*/
+   $('html').css('background', 'url(assets/images/'+ponerFondoSegunHora()+'.jpg) no-repeat center center fixed');
 
 })
+
+function ponerFondoSegunHora(){
+  var img = "";
+  var temps = new Date();
+  var hora = temps.getHours();
+
+  if ((hora > 19 && hora <= 23) || (hora < 6)){
+    return "vespre";
+  }else if (hora > 6 && hora < 10) {
+    return "sortida";
+  }else if (hora > 10 && hora < 17) {
+    return "dia";
+  }else if (hora > 17 && hora < 19) {
+    var num = Math.floor(Math.random() * 2) + 1;
+    return "posta"+num;
+  }else{
+    return false;
+  }
+}
